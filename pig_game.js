d7d9d677f13cceced59e8c1bfb95b3588ee9358b
Paul Ofili <paulofili42@gmail.com>
2018-10-14 14:50:47 +0100

    Added new files

--- pig_game.js
+++ pig_game.js
@@ -1,41 +1,71 @@
-var scores, activePlayer, roundScore, gamePlaying;
+var scores, activePlayer, roundScore, gamePlaying, dice;
 
-init();
+//window.open('main.js?cancelled=transaction has been cancelled','_self');  
+
+
+
+
+var total= init();
+
+document.querySelector('#totalValue').addEventListener('blur', function() {
+    total = document.getElementById('totalValue').value;
+    console.log(total)
+});
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-
+    
     if(gamePlaying==true) {
-
-        var dice = Math.floor(Math.random() * 6) + 1 ;
+        // console.log(dice);
+       if(!dice) {
+           prevDice=-1
+       } else {
+           var prevDice = dice;
+       }
+        
+        dice = Math.floor(Math.random() * 6) + 1 ;
+        
+        var dice2 = Math.floor(Math.random() * 6) + 1 ;
+        console.log(`${prevDice} and ${dice}`);
         
     
-        var diceDom = document.querySelector('.dice');
-        diceDom.src = `dice-${dice}.png`;
+        var diceDom = document.querySelector('.dice1');
+        diceDom.src = `images/dice-${dice}.png`;
         diceDom.style.display = 'block';
+        var diceDom2 = document.querySelector('.dice2');
+        diceDom2.src = `images/dice-${dice2}.png`;
+        diceDom2.style.display = 'block';
     
+        // if (dice=== 6 && prevDice=== 6) {
+        //     scores[activePlayer] = 0;
+        //     document.getElementById('score-' + activePlayer).textContent = 0;
+        //     nextPlayer();
+        // }
+
         if(dice !== 1) {
-            roundScore= roundScore + dice;
+            roundScore= roundScore + dice + dice2;
             // console.log(roundScore);
             document.querySelector('#current-' + activePlayer).innerHTML = `${roundScore}`;
         } else {
             nextPlayer();
         }
+
+        
     }
     
 });
-
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if(gamePlaying==true) {
         scores[activePlayer] += roundScore;
 
         document.querySelector('#score-' + activePlayer).innerHTML = `${scores[activePlayer]}`;
 
-        if(scores[activePlayer] >= 15) {
-            console.log("Magic");
+        if(scores[activePlayer] >= total) {
+            // console.log("Magic");
             document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
             document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
             document.querySelector(`#name-${activePlayer}`).innerHTML='Winner';
-            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice1').style.display = 'none';
+            document.querySelector('.dice2').style.display = 'none';
             gamePlaying = false
         } else {
             nextPlayer();
@@ -52,16 +82,25 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';
 }
 
 function init() {
+
+    var total = 15;
+
+   
+
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
     gamePlaying = true;
+    
+   
 
-    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice1').style.display = 'none';
+    document.querySelector('.dice2').style.display = 'none';    
 
     document.getElementById('score-0').textContent = 0;
     document.getElementById('score-1').textContent = 0;
@@ -76,6 +115,8 @@ function init() {
     document.querySelector(`.player-0-panel`).classList.remove('winner');
     document.querySelector(`.player-1-panel`).classList.remove('winner');
     document.querySelector('.player-0-panel').classList.add('active');
+
+    return total;
 
 }
 
